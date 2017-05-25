34fbe6d9482fe678de7cfbdd8aea00ca27ca4ab9
Winston Chu <wpchu@andrew.cmu.edu>
2017-05-25 12:01:24 -0400

    added payment model and credit card files

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,32 @@
 class Payment < ActiveRecord::Base
+
+  require 'base64'
+
+  # Virtual attributes (non-saved)
+  attr_accessor :credit_card_number
+  attr_accessor :expiration_year
+  attr_accessor :expiration_month
+
+  # Relationships
+  has_one :payback
+  belongs_to :payee, class_name: "User", foreign_key: "payee_id"
+  belongs_to :payer, class_name: "User", foreign_key: "payer_id"
+  belongs_to :fundraiser
+
+  # Scopes
+  scope :for_fundraiser, ->(fundraiser_id) { where(fundraiser_id: fundraiser_id) }
+  scope :for_user,       ->(user_id) { where(user_id: user_id) }
+
+  # Methods
+  def pay
+    return false unless self.payment_receipt.nil?
+    generate_payment_receipt
+    self.save!
+    self.payment_receipt
+  end
+
+  def credit_card_type
+    credit_card.type.nil? ? "N/A" : credit_card.type.name
+  end
+
 end
