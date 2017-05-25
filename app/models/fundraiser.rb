1ace1a307f30687c07b0cadb3a1c4b070af768e7
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2017-05-25 13:58:52 -0400

    Fixed indentations

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -7,7 +7,7 @@ class Fundraiser < ActiveRecord::Base
   # Validations
   validates :fundraiser_not_a_duplicate, on: :create
   def already_exists? # Function to check if the fundraiser name exists in the database
-  Fundraiser.where(name: self.name) == 1 # Access the database for any fundraisers with the name, if there exists one already, returns true
+  		Fundraiser.where(name: self.name) == 1 # Access the database for any fundraisers with the name, if there exists one already, returns true
 	end
 
 	def fundraiser_not_a_duplicate # Function to be called for validation
