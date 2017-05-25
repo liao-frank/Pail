34e1f0347eabf1cc6ed37e0a63e2314d15d2d81b
Winston Chu <wpchu@andrew.cmu.edu>
2017-05-25 11:35:41 -0400

    added relationships for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+
+  # Log-in
+  has_secure_password
+
+  # Relationships
+  has_many :payments
+  has_many :fundraisers
+  has_many :organization_users
+  has_many :organizations, through: :organization_users
+  has_many :sent_payments, class_name: "Payment", foreign_key: "payee_id"
+  has_many :received_payments, class_name: "Payment", foreign_key: "payer_id"
+
+  # Scopes
+
 end
