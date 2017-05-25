180138a133642cba69186063aeb619702bae7b71
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2017-05-25 13:03:10 -0400

    Added error checking for if a fundraiser has the name taken already

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -3,4 +3,17 @@ class Fundraiser < ActiveRecord::Base
   belongs_to :user
   has_many :payments
 
+  	# Validations
+	validates :fundraiser_not_a_duplicate, on :create
+
+	def already_exists? # Function to check if the fundraiser name exists in the database
+		Fundraiser.where(:name=self) == 1 # Access the database for any fundraisers with the name, if there exists one already, returns true
+	end
+
+	def fundraiser_not_a_duplicate # Function to be called for validation
+		return true if self.name.nil? # Checks if the name is empty
+		if self.already_exists? # Check if a duplicate
+			errors.add(:name, " already exists as a fundraiser")
+		end
+	end
 end
