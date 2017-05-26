cd8a2968c9bb8846ca4f8a8e9a628b91cd0e7716
BenjaminCatarevas <benjamin.catarevas@uconn.edu>
2017-05-25 21:45:36 -0400

    Increased amount of fundraisers taken from database to 10 to line up with the amount of payments displayed on frontpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 	def home
 		@payments = Payment.chronological.to_a.take(10)
-		@fundraisers = Fundraiser.chronological.to_a.take(5)
+		@fundraisers = Fundraiser.chronological.to_a.take(10)
 	end
 
 	def about
