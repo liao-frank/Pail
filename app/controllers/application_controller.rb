27218f32674aef520ef2b5a4317b437559fbac71
recs7168 <recs7168@gmail.com>
2017-05-25 21:24:47 -0400

    image helpers moved to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,5 +17,35 @@ class ApplicationController < ActionController::Base
   def check_login
     redirect_to login_url, alert: "You need to log in to view this page." if current_user.nil?
   end
-    
+  
+  private
+  # Constants
+  FUNDRAISER_NAME_DIR = "fundraisers" # Path constant
+  USERIMAGE_DIR = "user-images" # Path constant
+
+  def getUserImagePath(username) # Obtain file path for a user's profile image
+    if Rails.application.assets.find_asset USERIMAGE_DIR + "/#{username}.jpg"
+      return USERIMAGE_DIR + "/#{username}.jpg"
+    elsif Rails.application.assets.find_asset USERIMAGE_DIR + "/#{username}.png"
+      return USERIMAGE_DIR + "/#{username}.png"
+    else
+      return USERIMAGE_DIR + "/no-image.jpg"
+    end
+  end
+  helper_method :getUserImagePath
+
+  def getFundraiserImagePath(fundraiser_name) # Obtain file path for a fundraiser's profile image
+    if Rails.application.assets.find_asset FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.jpg"
+      return FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.jpg"
+    elsif Rails.application.assets.find_asset FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.png"
+      return FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.png"
+    else
+      return FUNDRAISER_NAME_DIR + "/no-image.jpg"
+    end
+  end
+
+  def getImage(username) # Returns path to the image
+    username + ".jpg"
+  end
+  helper_method :getFundraiserImagePath
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,35 +15,4 @@ class HomeController < ApplicationController
 
 	def contact
 	end
-
-	private
-	# Constants
-	FUNDRAISER_NAME_DIR = "fundraisers" # Path constant
-	USERIMAGE_DIR = "user-images" # Path constant
-
-	def getUserImagePath(username) # Obtain file path for a user's profile image
-		if Rails.application.assets.find_asset USERIMAGE_DIR + "/#{username}.jpg"
-			return USERIMAGE_DIR + "/#{username}.jpg"
-		elsif Rails.application.assets.find_asset USERIMAGE_DIR + "/#{username}.png"
-			return USERIMAGE_DIR + "/#{username}.png"
-		else
-			return USERIMAGE_DIR + "/no-image.jpg"
-		end
-	end
-	helper_method :getUserImagePath
-
-	def getFundraiserImagePath(fundraiser_name) # Obtain file path for a fundraiser's profile image
-		if Rails.application.assets.find_asset FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.jpg"
-			return FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.jpg"
-		elsif Rails.application.assets.find_asset FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.png"
-			return FUNDRAISER_NAME_DIR + "/#{fundraiser_name}.png"
-		else
-			return FUNDRAISER_NAME_DIR + "/no-image.jpg"
-		end
-	end
-
-	def getImage(username) # Returns path to the image
-		username + ".jpg"
-	end
-	helper_method :getFundraiserImagePath
 end
